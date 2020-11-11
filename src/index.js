155a6c33280366241101d9a8b7b935b9e4cfb597
Yuliia Bevko <bevko.yulka@gmail.com>
2020-11-11 23:50:33 +0200

    task-3-ready

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './css/styles.scss';
 
-// import { delay, logger } from './js/index-1';
+import { delay, logger } from './js/index-1';
 // import { users, toggleUserState, logger } from './js/index-2';
-import { randomIntegerFromInterval, makeTransaction, logSuccess, logError } from './js/index-3';
+// import { randomIntegerFromInterval, makeTransaction, logSuccess, logError } from './js/index-3';
--- src/js/index-1.js
+++ src/js/index-1.js
@@ -1,15 +1,15 @@
-// export { delay, logger };
+export { delay, logger };
 
 
-// const delay = ms => {
-//  return new Promise(resolve => {
-//     setTimeout(() => resolve(ms), ms);
-//   });
-// };
+const delay = ms => {
+ return new Promise(resolve => {
+    setTimeout(() => resolve(ms), ms);
+  });
+};
 
-// const logger = time => console.log(`Resolved after ${time}ms`);
+const logger = time => console.log(`Resolved after ${time}ms`);
 
-// // Вызовы функции для проверки
-// delay(2000).then(logger); // Resolved after 2000ms
-// delay(1000).then(logger); // Resolved after 1000ms
-// delay(1500).then(logger); // Resolved after 1500ms
+// Вызовы функции для проверки
+delay(2000).then(logger); // Resolved after 2000ms
+delay(1000).then(logger); // Resolved after 1000ms
+delay(1500).then(logger); // Resolved after 1500ms
