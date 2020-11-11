e8fa23223ad9c701f20bc48d90669bb5d00d87fd
Yuliia Bevko <bevko.yulka@gmail.com>
2020-11-11 23:46:08 +0200

    home-task-3

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './css/styles.scss';
+
+// import { delay, logger } from './js/index-1';
+// import { users, toggleUserState, logger } from './js/index-2';
+import { randomIntegerFromInterval, makeTransaction, logSuccess, logError } from './js/index-3';
